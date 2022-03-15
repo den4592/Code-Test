85e1260837f233269b8f2d39a97fcaff247065d9
ilya <den4592@naver.com>
2022-03-16 01:17:30 +0900

    블랙잭

---
+++ 시뮬레이션/블랙잭.py
@@ -0,0 +1,10 @@
+n,m=map(int,input().split())
+data=list(map(int,input().split()))
+result=0
+
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            if data[i]+data[j]+data[k]<=m:
+                result=max(result,data[i]+data[j]+data[k])
+print(result)
