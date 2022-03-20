64c1a2472def91713f812f6c589e8ea567e13b6a
ilya <den4592@naver.com>
2022-03-20 22:34:31 +0900

    단비 번호 붙이기

---
+++ DFS,BFS/단지번호붙이기.py
@@ -0,0 +1,32 @@
+n=int(input())
+graph=[]
+for _ in range(n):
+    graph.append(list(map(int,input())))
+
+grp=[]
+cnt=0
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+def dfs(x,y):
+    global cnt
+    if x<0 or x>=n or y<0 or y>=n:
+        return False
+
+    if graph[x][y]==1: #1일 때
+        cnt+=1  #카운트를 1 올리고
+        graph[x][y]=0 #방문한 곳을 0으로 변경 
+        for i in range(4):
+            dfs(x+dx[i],y+dy[i]) 
+        return True
+
+for i in range(n): #graph를 돌면서
+    for j in range(n): 
+        if dfs(i,j)==True: #다 돌고 True를 반환하면
+            grp.append(cnt) #grp에 cnt를 추가
+            cnt=0 #cnt 값 초기화
+
+print(len(grp))
+grp.sort()
+for i in grp:
+    print(i)
--- DFS,BFS/미로탐색.py
+++ DFS,BFS/미로탐색.py
@@ -13,7 +13,7 @@ def bfs(x,y):
     queue.append((x,y))
     while queue:
         x,y=queue.popleft()
-        for i in range(4):
+        for i in range(4): #동서남북 4면 체크
             nx=x+dx[i]
             ny=y+dy[i]
             if nx<0 or ny<0 or nx>=n or ny>=m:
@@ -21,9 +21,9 @@ def bfs(x,y):
             if graph[nx][ny]==0:
                 continue
             if graph[nx][ny]==1:
-                graph[nx][ny]=graph[x][y]+1
+                graph[nx][ny]=graph[x][y]+1 #지점까지의 거리를 기존 위치까지의 거리+1로 설정
                 queue.append((nx,ny))
-    return graph[n-1][m-1]
+    return graph[n-1][m-1] #맨 아랫줄에 위치하게 되면 리턴
 
 print(bfs(0,0))
 #알고리즘 책 미로탈출이랑 똑같은 문제
