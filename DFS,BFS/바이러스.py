c9d58e873bac51db9c8830eb33404b5e5ec7fde7
ilya <den4592@naver.com>
2022-03-24 23:53:58 +0900

    우유 축제

--- DFS,BFS/바이러스.py
+++ DFS,BFS/바이러스.py
@@ -7,14 +7,17 @@ for _ in range(m):							# 연결된 컴퓨터 쌍의 수만큼 반복
     x, y = map(int, input().split())		
     graph[x].append(y)
     graph[y].append(x)
+print(graph)
 
 visited = [0] * (n+1)	# 방문처리 : 방문한 컴퓨터 수를 출력해야하므로 visited 에 True/False가 아닌 0/1로 처리
 
-def dfs(graph, v, visited):
+
+def dfs(v): #1-2-3-5-6까지만 4-7 무시
     visited[v] = 1
     for i in graph[v]:
-        if visited[i] == 0:
-            dfs(graph, i, visited)
+        if not visited[i]:
+            dfs(i)
 
-dfs(graph, 1, visited)
+dfs(1)
+print(visited)
 print(sum(visited)-1)	
